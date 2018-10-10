536b0e315deb0ac03bdc256c10c1b800fb23ac9e
Sachin Titus <sachinottawa@gmail.com>
2018-10-10 03:40:27 -0700

    added synchronized singleton

---
+++ src/SynchronizedSingleton.java
@@ -0,0 +1,29 @@
+
+
+public class SynchronizedSingleton {
+
+    private static SynchronizedSingleton synchronizedSingleton;
+
+    private SynchronizedSingleton(){
+        System.out.println("Creation happens only once");
+    }
+
+    static synchronized SynchronizedSingleton getInstance(){
+        if(synchronizedSingleton == null){
+            synchronizedSingleton = new SynchronizedSingleton();
+            return synchronizedSingleton;
+        }
+        return synchronizedSingleton;
+    }
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            SynchronizedSingleton singleton = SynchronizedSingleton.getInstance();
+        });
+        Thread thread2 = new Thread(() -> {
+            SynchronizedSingleton singleton = SynchronizedSingleton.getInstance();
+        });
+        thread1.start();
+        thread2.start();
+    }
+}
