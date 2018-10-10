394d7e3a33f31f306bcabd6c52d603932171180d
Sachin Titus <sachinottawa@gmail.com>
2018-10-10 03:25:34 -0700

    Added comments

--- src/SimpleSingleton.java
+++ src/SimpleSingleton.java
@@ -3,6 +3,13 @@
  * as the constructor is given private
  */
 public class SimpleSingleton {
+    /**
+     * private static SimpleSingleton simpleSingleton = new SimpleSingleton();
+     *
+     * The above line also works instead of the line we have here. It is called eager initialization.
+     * To put it simply, the object is created before even getInstance() is invoked.
+     * What we have here is called lazy initialization
+     */
     private static SimpleSingleton simpleSingleton;
     private SimpleSingleton(){
         System.out.println("Creation happens only once");
